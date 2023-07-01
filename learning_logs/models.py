0581ce4e320e1d04b6b03890cde015b4b39d51ae
ximena9201 <ximenandrea.ro@gmail.com>
2023-07-01 08:41:49 -0400

    fix entry

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -16,7 +16,7 @@ class Topic(models.Model):
 class Entry(models.Model):
     """Something specific learned about a topic"""
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
-    text = models.TextField
+    text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
     
     
